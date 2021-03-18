50e103868c191aa4a592845e1adc619aad9eb966
Utsav Kapoor <utsav@grappus.com>
2021-03-18 11:55:37 +0530

    Linkedlist

--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -70,6 +70,7 @@ class LinkedList {
     return current;
   }
 
+  
   insert( index, value ) {
 
     if( index >= this.length ) {
@@ -94,11 +95,7 @@ module.exports = {
   LinkedList
 };
 
-// let myLinkedList = new LinkedList( 10 );
-// myLinkedList.append( 5 );
-// myLinkedList.append( 16 ); myLinkedList.prepend( 1 );
-// myLinkedList.insert( 2, 99 );
-// myLinkedList.insert( 20, 88 );
+
 
 
 
